2ac5775d3e2cee4485133e4c52dbd5278cd91a28
Khải Võ <kenkaneki395@gmail.com>
2026-01-17 20:29:23 +0700

    feat: Allow resizable window with minimum size and anchor chess board panel

--- GameCaro/Form1.Designer.cs
+++ GameCaro/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.pctbMark = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -38,7 +37,6 @@
             this.btnLAN = new System.Windows.Forms.Button();
             this.txbIP = new System.Windows.Forms.TextBox();
             this.txbPlayerName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.lblCountO = new System.Windows.Forms.Label();
             this.lblCountX = new System.Windows.Forms.Label();
             this.pnlChessBoard = new System.Windows.Forms.Panel();
@@ -48,27 +46,15 @@
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(784, 49);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(310, 299);
-            this.panel1.TabIndex = 1;
-            // 
             // panel4
             // 
-            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.panel4.BackColor = System.Drawing.SystemColors.Control;
             this.panel4.Controls.Add(this.pctbMark);
             this.panel4.Controls.Add(this.label5);
@@ -76,9 +62,9 @@
             this.panel4.Controls.Add(this.btnLAN);
             this.panel4.Controls.Add(this.txbIP);
             this.panel4.Controls.Add(this.txbPlayerName);
-            this.panel4.Location = new System.Drawing.Point(784, 354);
+            this.panel4.Location = new System.Drawing.Point(921, 426);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(310, 243);
+            this.panel4.Size = new System.Drawing.Size(331, 243);
             this.panel4.TabIndex = 3;
             // 
             // pctbMark
@@ -86,9 +72,9 @@
             this.pctbMark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pctbMark.BackColor = System.Drawing.SystemColors.Control;
             this.pctbMark.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pctbMark.Location = new System.Drawing.Point(170, 5);
+            this.pctbMark.Location = new System.Drawing.Point(191, 5);
             this.pctbMark.Name = "pctbMark";
-            this.pctbMark.Size = new System.Drawing.Size(134, 141);
+            this.pctbMark.Size = new System.Drawing.Size(137, 131);
             this.pctbMark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pctbMark.TabIndex = 3;
             this.pctbMark.TabStop = false;
@@ -98,7 +84,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label5.Location = new System.Drawing.Point(3, 149);
+            this.label5.Location = new System.Drawing.Point(8, 151);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(294, 75);
             this.label5.TabIndex = 8;
@@ -138,23 +124,12 @@
             this.txbPlayerName.Size = new System.Drawing.Size(144, 20);
             this.txbPlayerName.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label1.Location = new System.Drawing.Point(11, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 22);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "O đi trước";
-            // 
             // lblCountO
             // 
             this.lblCountO.AutoSize = true;
             this.lblCountO.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCountO.ForeColor = System.Drawing.Color.OrangeRed;
-            this.lblCountO.Location = new System.Drawing.Point(888, 24);
+            this.lblCountO.Location = new System.Drawing.Point(432, 24);
             this.lblCountO.Name = "lblCountO";
             this.lblCountO.Size = new System.Drawing.Size(43, 22);
             this.lblCountO.TabIndex = 6;
@@ -165,7 +140,7 @@
             this.lblCountX.AutoSize = true;
             this.lblCountX.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCountX.ForeColor = System.Drawing.Color.OrangeRed;
-            this.lblCountX.Location = new System.Drawing.Point(952, 24);
+            this.lblCountX.Location = new System.Drawing.Point(496, 24);
             this.lblCountX.Name = "lblCountX";
             this.lblCountX.Size = new System.Drawing.Size(43, 22);
             this.lblCountX.TabIndex = 7;
@@ -173,10 +148,13 @@
             // 
             // pnlChessBoard
             // 
+            this.pnlChessBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlChessBoard.BackColor = System.Drawing.SystemColors.Control;
             this.pnlChessBoard.Location = new System.Drawing.Point(15, 49);
             this.pnlChessBoard.Name = "pnlChessBoard";
-            this.pnlChessBoard.Size = new System.Drawing.Size(754, 548);
+            this.pnlChessBoard.Size = new System.Drawing.Size(900, 620);
             this.pnlChessBoard.TabIndex = 8;
             // 
             // tmCoolDown
@@ -190,7 +168,7 @@
             this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1122, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
             this.menuStrip1.TabIndex = 9;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -228,36 +206,33 @@
             this.quitToolStripMenuItem.Text = "Quit";
             this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
             // 
-            // label2
+            // panel1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label2.Location = new System.Drawing.Point(661, 24);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(153, 22);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Số nước đã đi";
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::GameCaro.Properties.Resources.banner;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(915, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 92);
+            this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1122, 610);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.lblCountX);
             this.Controls.Add(this.lblCountO);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pnlChessBoard);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "CARO";
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.panel4.ResumeLayout(false);
@@ -277,7 +252,6 @@
         private System.Windows.Forms.TextBox txbIP;
         private System.Windows.Forms.TextBox txbPlayerName;
         private System.Windows.Forms.ProgressBar prcbCoolDown;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblCountO;
         private System.Windows.Forms.Label lblCountX;
         private System.Windows.Forms.Label label5;
@@ -289,7 +263,6 @@
         private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
-        private System.Windows.Forms.Label label2;
     }
 }
 
